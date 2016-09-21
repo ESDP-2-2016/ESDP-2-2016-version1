5208ec6edc49dff44f1dbc141f286f6acfa913fc
ArsGilani <gilani.vu@gmail.com>
2016-09-21 20:49:22 +0600

    change column from postcategory to post_category  in Post table

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :organization
   belongs_to :category
+  belongs_to :post_category
 end
--- app/models/post_category.rb
+++ app/models/post_category.rb
@@ -1,2 +1,3 @@
 class PostCategory < ApplicationRecord
+  has_many :posts
 end
