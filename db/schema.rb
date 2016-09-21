0e418d0872fc3d09a13b73305eb0759c591a96f7
Olga <gudovaolya@gmail.com>
2016-09-21 17:41:07 +0600

    add test login.feature

--- db/schema.rb
+++ db/schema.rb
@@ -134,8 +134,6 @@ ActiveRecord::Schema.define(version: 20160920093628) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "provider"
-    t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,20 @@
+When(/^я на странице "\/users\/sign_in"$/) do
+  visit new_user_session_path
+end
+
+When(/^ввожу "([^"]*)" в поле "([^"]*)"$/) do |value, field|
+  fill_in field, with:value
+end
+
+When(/^кликаю на кнопку "([^"]*)"$/) do |button|
+  click_button button
+end
+
+When(/^должен перейти на главную страницу$/) do
+  visit root_path
+end
+
+When(/^должен увидеть текст "([^"]*)"$/) do |text|
+  page.should have_content(text)
+  sleep(3)
+end
