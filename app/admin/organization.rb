5e3ee14c11cbaae7982df884abb0338faa8d2cc7
Maria <tsvetkovamariia@gmail.com>
2016-09-23 16:15:44 +0600

    merged branches woth open street map

---
+++ app/admin/organization.rb
@@ -0,0 +1,60 @@
+ActiveAdmin.register Organization do
+
+  permit_params :name, :location, :phone, :address, :contact_person,
+                :longitude, :latitude, :organization_category_id, :oblast_id, :url, :active
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :oblast_id,  :as => :select,      :collection => Oblast.all
+      f.input :location
+      f.input :address
+      f.input :url
+      f.input :longitude
+      f.input :latitude
+      f.input :organization_category_id,  :as => :select,  :collection => OrganizationCategory.all
+      f.input :active, :as => :radio
+    end
+    f.actions
+  end
+
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :oblast_id do |organization|
+      organization.oblast.name
+    end
+    column :location
+    column :address
+    column :contact_person
+    column :phone
+    column :url
+    column :longitude
+    column :latitude
+    column :organization_category_id do |organization|
+      organization.organization_category.name
+    end
+    column :active
+    actions
+  end
+
+
+
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+
+end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,11 +1,21 @@
 class Organization < ApplicationRecord
 
+  after_initialize :set_defaults, unless: :persisted?
+
   belongs_to :organization_category
   belongs_to :oblast
+
+  has_many :user_organizations
+  has_many :users, :through => :user_organizations
 
   validates :name, presence: true, length: {maximum: 250}
   validates :oblast_id, presence: true
   validates :location, presence: true 
   validates :organization_category_id, presence: true
+
+
+  def set_defaults
+    self.active = false if self.active.nil?
+  end
 
 end
