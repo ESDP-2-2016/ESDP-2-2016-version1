5c1ec825b99e1d4e8f00b77d2dbdf074f2f30754
Maxim <karamba92@mail.ru>
2016-09-30 17:55:45 +0600

    Добавил permit params в Post.rb post_category.rb, поправил user/show.html

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,5 +1,9 @@
 ActiveAdmin.register Post do
 
+  permit_params :title, :body, :open, :organization_id, :post_category_id,
+                :user_id
+
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
--- app/admin/post_category.rb
+++ app/admin/post_category.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register PostCategory do
 
+permit_params :name
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
