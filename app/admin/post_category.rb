5dbc73c52cd50f537e1e318f809c93f8d71c58c0
Maxim <karamba92@mail.ru>
2016-09-30 18:06:24 +0600

    Решаем конфликт post_category

--- app/admin/post_category.rb
+++ app/admin/post_category.rb
@@ -1,10 +1,6 @@
 ActiveAdmin.register PostCategory do
 
-<<<<<<< HEAD
-=======
 permit_params :name
-
->>>>>>> 5c1ec825b99e1d4e8f00b77d2dbdf074f2f30754
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
