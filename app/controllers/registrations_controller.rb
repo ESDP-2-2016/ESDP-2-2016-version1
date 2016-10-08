51f818145ebc09f992ecb256ef682c49d45adb3a
Maxim <karamba92@mail.ru>
2016-10-08 11:50:16 +0600

    #69 исправил ошибки в(в переменных) RegistrationController

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,7 @@ class RegistrationsController < Devise::RegistrationsController
     @owner_organizations = UserOrganization.where(user_id: @user.id, role: 1)
     @participant_organizations = UserOrganization.where(user_id: @user.id, role: 2)
     @aids = Aid.where(user_id: @user.id)
-    @organization = Organization.find(params[:id])
+    @organization = UserOrganization.find(@user)
   end
 
   def create
@@ -14,7 +14,6 @@ class RegistrationsController < Devise::RegistrationsController
       role = 2
       o_user = @user.id
       organization = 1
-      # @user_organization.save
       @user_organization = UserOrganization.create!(role: role, user_id: o_user, organization_id: organization)
       redirect_to root_path
     else
