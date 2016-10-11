1dc9c7dc7361185d6155620738c0bd9d48852b6b
Omurbek Choibekov <omka3s@gmail.com>
2016-10-11 17:29:36 +0600

    #57 geocoder gem installed some links translated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,29 @@ class ApplicationController < ActionController::Base
     if cookies[:educator_locale] && I18n.available_locales.include?(cookies[:educator_locale].to_sym)
       l = cookies[:educator_locale].to_sym
     else
-      l = I18n.default_locale
-      cookies.permanent[:educator_locale] = l
+      begin
+        country_code = request.location.country_code
+        if country_code
+          country_code = country_code.downcase.to_sym
+          # use russian for CIS countries, english for others
+          [:ru, :kz, :ua, :by, :tj, :uz, :md, :az, :am, :kg, :tm].include?(country_code) ? l = :ru : l = :en
+        else
+          l = I18n.default_locale # use default locale if cannot retrieve this info
+        end
+      rescue
+        l = I18n.default_locale
+      ensure
+        cookies.permanent[:educator_locale] = l
+      end
     end
     I18n.locale = l
+  
+  #   if cookies[:educator_locale] && I18n.available_locales.include?(cookies[:educator_locale].to_sym)
+  #     l = cookies[:educator_locale].to_sym
+  #   else
+  #     l = I18n.default_locale
+  #     cookies.permanent[:educator_locale] = l
+  #   end
+  #   I18n.locale = l
   end
 end
