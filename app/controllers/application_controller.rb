2ae1e088ffe1df3a7f0c5a023c9d3bc1429d11b8
Omurbek Choibekov <omka3s@gmail.com>
2016-10-12 17:01:56 +0600

    #57 set default locale via browser language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,31 +11,45 @@ class ApplicationController < ActionController::Base
     if cookies[:educator_locale] && I18n.available_locales.include?(cookies[:educator_locale].to_sym)
       l = cookies[:educator_locale].to_sym
     else
-      begin
-        country_code = request.location.country_code
-        if country_code
-          country_code = country_code.downcase.to_sym
-          # use russian for CIS countries, english for others
-          [:ru, :kz, :ua, :by, :tj, :uz, :md, :az, :am, :kg, :tm,:rd].include?(country_code) ? l = :ru : l = :en
-        else
-          l = I18n.default_locale # use default locale if cannot retrieve this info
-        end
-      rescue
-        l = I18n.default_locale
-      ensure
+      I18n.locale = extract_locale_from_accept_language_header
         cookies.permanent[:educator_locale] = l
-      end
+
     end
     I18n.locale = l
-
-    # if cookies[:educator_locale] && I18n.available_locales.include?(cookies[:educator_locale].to_sym)
-    #   l = cookies[:educator_locale].to_sym
-    # else
-    #   l = I18n.default_locale
-    #   cookies.permanent[:educator_locale] = l
-    # end
-    # I18n.locale = l
   end
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
+  # def set_locale
+  #   if cookies[:educator_locale] && I18n.available_locales.include?(cookies[:educator_locale].to_sym)
+  #     l = cookies[:educator_locale].to_sym
+  #   else
+  #     begin
+  #       country_code = request.location.country_code
+  #       if country_code
+  #         country_code = country_code.downcase.to_sym
+  #         # use russian for CIS countries, english for others
+  #         [:ru, :kz, :ua, :by, :tj, :uz, :md, :az, :am, :kg, :tm].include?(country_code) ? l = :ru : l = :en
+  #       else
+  #         l = I18n.default_locale # use default locale if cannot retrieve this info
+  #       end
+  #     rescue
+  #       l = I18n.default_locale
+  #     ensure
+  #       cookies.permanent[:educator_locale] = l
+  #     end
+  #   end
+  #   I18n.locale = l
+  # end
+
+  #   if cookies[:educator_locale] && I18n.available_locales.include?(cookies[:educator_locale].to_sym)
+  #     l = cookies[:educator_locale].to_sym
+  #   else
+  #     l = I18n.default_locale
+  #     cookies.permanent[:educator_locale] = l
+  #   end
+  #   I18n.locale = l
+  # end
   def set_admin_locale
     I18n.locale = :ru
   end
