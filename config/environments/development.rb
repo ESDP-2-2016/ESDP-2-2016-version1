b953845fdd61ad382205b62cd6c6a95c43cdddff
Maxim <karamba92@mail.ru>
2016-10-20 17:39:00 +0600

    deploy test maler edit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'http://46.101.108.2' }
 
   config.action_mailer.delivery_method = :smtp
 
