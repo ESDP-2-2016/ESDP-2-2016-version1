c5735ca027954a2ecc1e6d93b7f3cb54da3ce795
Maxim <karamba92@mail.ru>
2016-10-20 18:16:03 +0600

    #87 deploy test

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,6 +8,7 @@ class UserMailer < ApplicationMailer
     @org = org
     @id = id
     @url  = 'http://http://46.101.108.2/admin/organizations/' + @id.to_s + '/edit'
+    @url1  = 'http://http://localhost:3000/admin/organizations/' + @id.to_s + '/edit'
     mail(to: user.email, subject: 'Welcome to My Awesome Site')
   end
   def notification(user,id,org)
