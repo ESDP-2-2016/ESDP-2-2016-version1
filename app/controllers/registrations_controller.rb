62272e4da5a4cc7373c301111acf41874e7ff660
Omurbek Choibekov <omka3s@gmail.com>
2016-10-27 14:47:43 +0600

    #76 some changes in registration controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,38 +3,17 @@ class RegistrationsController < Devise::RegistrationsController
   def show
     @user = User.find(params[:id])
     @owner_organizations = UserOrganization.where(user_id: @user.id, role: 1)
-    # @massiv=[]
-    #
-    # @owner_organizations.each do |item|
-    #   qw=item.organization_id
-    #   @massiv.push qw
-    # end
-
     @requests=[]
-    # @massiv.each do |id|
-    #   @request= UserOrganization.where(organization_id:id,role:2,approved:false)
-    #     @request.each do |item|
-    #       @requests.push item
-    #     end
-    # end
-    #
-
     @owner_organizations.each do |test|
       @request= UserOrganization.where(organization_id:test.organization_id,role:2,approved:false)
       @request.each do |item|
         @requests.push item
       end
     end
-
-
-
-
       @participant_organizations = UserOrganization.where(user_id: @user.id, role: 2, approved: true)
-    @waiting_for_approval = UserOrganization.where(user_id: @user.id, role: 2, approved: false)
-    @aids = Aid.where(user_id: @user.id)
+      @waiting_for_approval = UserOrganization.where(user_id: @user.id, role: 2, approved: false)
+      @aids = Aid.where(user_id: @user.id)
   end
-  #<UserOrganization id: 19, role: 2, user_id: 1, organization_id: 7, created_at:
-  # "2016-10-26 09:53:03", updated_at: "2016-10-26 09:53:03", approved: false>
 
 
   def create
@@ -43,7 +22,7 @@ class RegistrationsController < Devise::RegistrationsController
       role = 2
       o_user = @user.id
       organization = 1
-      @user_organization = UserOrganization.create!(role: role, user_id: o_user, organization_id: organization)
+      @user_organization = UserOrganization.create!(role: role, user_id: o_user, organization_id: organization,approved:true)
 
     flash[:success] = 'Вы успешно зарегистрировались!'
     else
