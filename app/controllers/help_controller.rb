9845887363c3f405b8319c800ef9adff3b6e017c
Omurbek Choibekov <omka3s@gmail.com>
2016-10-31 18:39:01 +0600

    #103 help page created

---
+++ app/controllers/help_controller.rb
@@ -0,0 +1,6 @@
+class HelpController < ApplicationController
+  def index
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get 'about/index'
+  get 'help/index'
   get '/change_locale/:locale', to: 'settings#change_locale', as: :change_locale
   root 'organizations#index'
 
