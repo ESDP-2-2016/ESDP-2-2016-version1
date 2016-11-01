de123c65612837f23999b702dd639019381eaaf9
Omurbek Choibekov <omka3s@gmail.com>
2016-11-01 18:47:25 +0600

    #100 config/env/prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-    
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
